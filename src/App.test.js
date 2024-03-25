debd7914ff7bcc929ac4081a5361bb8989dc689e
GardoKirsipuu <77277733+GardoKirsipuu@users.noreply.github.com>
2024-03-26 01:20:04 +0200

    feat: add city into weather list
    test: add tests for adding city to list and showing detailed city results

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, within } from '@testing-library/react';
 import App from './App';
 
 import { createMockServer } from './createMockServer';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
 describe('Weather Application tests', () => {
   let server;
@@ -29,5 +30,37 @@ describe('Weather Application tests', () => {
     userEvent.click(button);
 
     await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5));
+  });
+
+  it('shows city search result details', async () => {
+    render(<App />);
+
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne');
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5));
+    expect(screen.getByText(/Melbourne, -37.8142176, 144.9631608/i)).toBeInTheDocument();
+  }); 
+
+  it('add search result to my weather list', async () => {
+    render(<App />);
+
+    const input = screen.getByTestId('search-input');
+    userEvent.type(input, 'Melbourne');
+
+    const button = screen.getByTestId('search-button');
+    userEvent.click(button);
+
+    await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5));
+
+    const selected = screen.getAllByText(/Melbourne/i)[3];
+    act(() => {
+      userEvent.click(selected);
+    });
+
+    expect(within(screen.getByTestId('my-weather-list')).getByText(/Melbourne/i)).toBeInTheDocument();
   });
 });
